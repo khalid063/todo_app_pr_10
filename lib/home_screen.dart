2da89101f3e685ad4579f97e28d129355b24226d
KhalidPC <khalid.ahmad063@gmail.com>
2023-07-03 18:26:44 +0600

    Form Vlidation is done

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -19,6 +19,8 @@ class HomeScreenUI extends State<HomeScreen>{
 
   List<ToDo> todos = [];
 
+  GlobalKey<FormState> todoForm = GlobalKey<FormState>();    ///*** 3nd work for from validation
+
   /// BottomSheet open method for taking input
   void shoBottomSheetForTakingNewNote(){
     showModalBottomSheet(
@@ -26,34 +28,67 @@ class HomeScreenUI extends State<HomeScreen>{
         builder: (context){
           return Container(
             padding: const EdgeInsets.all(15),
-            child: Column(
-              children: [
-                Text("Add New Note", style: myTextStyle(),),
-                const SizedBox(height: 10,),
-                TextFormField(
-                  controller: _titleInputTakingController,
-                  decoration: myInputDecoretion("Title"),
-                ),
-                const SizedBox(height: 5,),
-                TextFormField(
-                  controller: _descriptionInputTakingController,
-                  decoration: myInputDecoretion("Description"),
-                ),
-                const SizedBox(height: 5,),
-                ElevatedButton(
-                  style: myElevatedButtonStyle(),
-                    onPressed: (){
-                    if(_titleInputTakingController.text.trim().isNotEmpty && _descriptionInputTakingController.text.trim().isNotEmpty){
-                      todos.add(ToDo(_titleInputTakingController.text.trim(), _descriptionInputTakingController.text.trim(), false));
-                      if(mounted){
-                        setState(() {});
+            child: Form(                            ///*** 1 st work for form Validation
+              key: todoForm,                        ///*** 2 rd work for form Validation
+              child: Column(
+                children: [
+                  Text("Add New Note", style: myTextStyle(),),
+                  const SizedBox(height: 10,),
+                  TextFormField(
+                    controller: _titleInputTakingController,
+                    decoration: myInputDecoretion("Title"),
+                    ///==================================== Start  ///*** 4 th work for validation =====================///
+                    validator: (String? value){
+                        if(value?.trim().isEmpty ?? true){
+                          return "Please Enter your Title";
+                        }
+                    },
+                    ///==================================== End  ///*** 4 th work for validation =====================///
+                  ),
+                  const SizedBox(height: 5,),
+                  TextFormField(
+                    controller: _descriptionInputTakingController,
+                    decoration: myInputDecoretion("Description"),
+                    ///==================================== Start  ///*** 6 th work for validation =====================///
+                    validator: (String? value){
+                      if(value?.trim().isEmpty ?? true){
+                        return "Please Enter your Description";
                       }
-                      _titleInputTakingController.clear();
-                      _descriptionInputTakingController.clear();
-                      Navigator.pop(context);
-                    }
-                    }, child: const Text("Add Note"))
-              ],
+                    },
+                    ///==================================== End  ///*** 6 th work for validation =====================///
+                  ),
+                  const SizedBox(height: 5,),
+                  ElevatedButton(
+                    style: myElevatedButtonStyle(),
+
+                      /// ============== ///*** 5th work for Form validation (New Code) ======================///
+                      onPressed: (){
+                         if(todoForm.currentState!.validate()){
+                           todos.add(ToDo(_titleInputTakingController.text.trim(), _descriptionInputTakingController.text.trim(), false));
+                             if(mounted){
+                               setState(() {});
+                             }
+                             _titleInputTakingController.clear();
+                             _descriptionInputTakingController.clear();
+                             Navigator.pop(context);
+                         }
+                      },child: const Text("Add Note"))
+
+                      /// ============== validation Old Code ======================///
+                      // onPressed: (){
+                      // if(_titleInputTakingController.text.trim().isNotEmpty && _descriptionInputTakingController.text.trim().isNotEmpty){
+                      //   todos.add(ToDo(_titleInputTakingController.text.trim(), _descriptionInputTakingController.text.trim(), false));
+                      //   if(mounted){
+                      //     setState(() {});
+                      //   }
+                      //   _titleInputTakingController.clear();
+                      //   _descriptionInputTakingController.clear();
+                      //   Navigator.pop(context);
+                      // }
+                      // }, child: const Text("Add Note"))
+                  /// ============== validation Old Code ======================///
+                ],
+              ),
             ),
           );
         }
@@ -87,7 +122,12 @@ class HomeScreenUI extends State<HomeScreen>{
               },
               title: Text("${todos[index].title}"),
               subtitle: Text("${todos[index].description}"),
-              leading: todos[index].isDone ? Icon(Icons.check_circle_rounded) : Icon(Icons.close),
+              //leading: todos[index].isDone ? Icon(Icons.check_circle_rounded) : Icon(Icons.close),   ///*** Old Code
+              leading: Visibility(                                                                     /// New Code of visibility
+                visible: todos[index].isDone,
+                replacement: Icon(Icons.check_circle_rounded),
+                child: Icon(Icons.close),
+              ),
               trailing: IconButton(
                 icon: Icon(Icons.delete),
                 onPressed: (){
